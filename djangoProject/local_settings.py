b5043859c19c3dbafb5d6ceb79fef4bcfcb1bbc4
小波 <xiaokaling@qq.com>
2024-04-25 11:33:19 +0800

    小波第三次提交

---
+++ djangoProject/local_settings.py
@@ -0,0 +1,13 @@
+
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demo11',
+        'USER': 'root',
+        'PASSWORD': '123321',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+    }
+}
